b5e6928a66264af1322ba1d0f0c5c82b40dc2082
Carlos V <cavarod@gmail.com>
2021-06-21 17:11:12 -0700

    repo initialization

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const path = require('path');
+
+const config = {
+
+}
+
+module.exports = config;
